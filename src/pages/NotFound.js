bc7647e3aa643c91e4741c3e603593fc527ff824
nao <nurettinakinoral@gmail.com>
2022-08-04 18:03:44 +0300

    Not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -7,37 +7,56 @@ import { Button, CardActionArea, CardActions } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div style={{display:'grid', placeItems:'center', marginTop:'3rem'}}>
-    <Card sx={{ maxWidth: 345, marginTop:'4rem'}}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="https://i.imgflip.com/3b0s3p.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Page Not Found
-          </Typography>
-            <p>Below are the possible reasons of this error.</p>
+    <div
+      style={{
+        display: 'grid',
+        placeItems: 'center',
+        marginTop: '3rem',
+      }}
+    >
+      <Card sx={{ maxWidth: 345, marginTop: '4rem', bgcolor: '#073B4C' }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="250"
+            image="https://cdn5.vectorstock.com/i/1000x1000/73/49/404-error-page-not-found-miss-paper-with-white-vector-20577349.jpg"
+            alt="Not found"
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ color: '#EF476F', fontSize: 32, fontWeight: 700 }}
+            >
+              Page Not Found
+            </Typography>
+            <p className="color">
+              Below are the possible reasons for this error to occur.
+            </p>
             <ul>
-                <li>Wrong URL</li>
-                <li>Unstable Internet connection</li>
-                <li>API Connection problems</li>
+              <li className="color">Wrong URL</li>
+              <li className="color">Post might be deleted</li>
+              <li className="color">Unstable internet connection</li>
+              <li className="color">Database connection problems</li>
             </ul>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" variant='contained' color="secondary" onClick={()=> navigate('/')}>
-          Home
-        </Button>
-      </CardActions>
-    </Card>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button
+            size="small"
+            variant="contained"
+            sx={{ color: '#9E0059', bgcolor: '#95D5B2' }}
+            onClick={() => navigate('/')}
+          >
+            Home
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
 
-export default NotFound
+export default NotFound;
